8fcd046c0f9c6aebbf5875247083a0334a56c1dc
Bryan <bryan@punknown.net>
2024-11-24 01:23:39 +0100

    Add API interactions with Spotify and Instagram
    This commit adds new modules for Spotify and Instagram in `src/api/`. The Spotify module handles OAuth authentication and provides methods to check the currently playing song. The Instagram module allows login through session or credentials and provides methods to get and set the user's biography.

---
+++ src/api/instagram.py
@@ -0,0 +1,88 @@
+"""Provide classes and methods for authenticating and interacting with Instagram."""
+
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+from instagrapi import Client  # type: ignore
+from instagrapi.exceptions import LoginRequired  # type: ignore
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Instagram:
+    """Handles interaction with Instagram through an authenticated client.
+
+    This class provides methods to log in to Instagram using either session
+    information or user's credentials, and to get and set the biography
+    of the logged-in user's account.
+
+    """
+
+    client: Client = Client()
+
+    def __post_init__(self) -> None:
+        """Initialize the Instagram client."""
+        self.client.delay_range = [1, 3]
+
+    def login(self, username: str, password: str) -> None:
+        """Authenticate with Instagram."""
+        session = self.client.load_settings(Path("session.json"))
+
+        login_via_session = False
+        login_via_credentials = False
+
+        if session:
+            try:
+                self.client.set_settings(session)
+                self.client.login(username=username, password=password)
+
+                try:
+                    self.client.get_timeline_feed()
+                except LoginRequired:
+                    logger.info("Session is invalid, logging in via credentials")
+                    old_session = self.client.get_settings()
+
+                    self.client.set_settings({})
+                    self.client.set_uuids(old_session["uuids"])
+
+                    self.client.login(username=username, password=password)
+                login_via_session = True
+            except Exception as e:
+                logger.info(f"Couldn't login user using session information: {e}")
+
+        if not login_via_session:
+            try:
+                logger.info("Attempting to login via credentials")
+                if self.client.login(username=username, password=password):
+                    login_via_credentials = True
+            except Exception as e:
+                logger.info(f"Couldn't login user using credentials: {e}")
+
+        if not login_via_session and not login_via_credentials:
+            raise Exception("Couldn't login user")
+
+    def get_current_biography(self) -> Optional[str]:
+        """Fetch the current biography of the logged-in user."""
+        try:
+            return self.client.account_info().biography
+        except LoginRequired:
+            logger.info("Please call login() before calling get_current_biography()")
+            return None
+        except Exception as e:
+            logger.info(f"Couldn't fetch biography: {e}")
+            return None
+
+    def set_current_biography(self, biography: str) -> None:
+        """Set the current biography of the logged-in user."""
+        if not biography:
+            raise Exception("Biography cannot be empty")
+        if self.get_current_biography() == biography:
+            return
+        if self.client.account_set_biography(biography):
+            logger.info("Biography updated successfully")
+        else:
+            logger.info("Couldn't update biography")
---
+++ src/api/spotify.py
@@ -0,0 +1,132 @@
+"""Provide classes and methods for authenticating and interacting with Spotify."""
+
+import logging
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+import spotipy  # type: ignore
+from spotipy.oauth2 import SpotifyOAuth  # type: ignore
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Spotify:
+    """Provides methods to authenticate and establish a connection with Spotify.
+
+    The `Spotify` class includes static and instance methods to support local and
+    server-based authentication workflows, making it flexible for different
+    deployment scenarios.
+
+    Attributes:
+    spotify: Optional[spotipy.Spotify]
+        An instance of Spotipy's Spotify client,
+         initialized after successful authentication.
+    spotify_oauth_manager: Optional[SpotifyOAuth]
+        An instance of SpotifyOAuth to manage the OAuth authentication process.
+
+    Methods:
+    local_create_spotify(client_id, client_secret, redirect_uri, scope):
+        Authenticates with Spotify and returns the access and refresh token.
+    server_create_spotify(client_id, client_secret, redirect_uri, scope, refresh_token):
+        Creates a Spotify server connection using the provided OAuth details.
+
+    """
+
+    spotify: Optional[spotipy.Spotify] = None
+    spotify_oauth_manager: Optional[SpotifyOAuth] = None
+
+    def local_create_spotify(
+        self, client_id: str, client_secret: str, redirect_uri: str, scope: str
+    ) -> Optional[Dict[str, str]]:
+        """Authenticate with Spotify and returns the access and refresh token.
+
+        Args:
+            client_id: str
+                The client ID provided by Spotify for authentication.
+            client_secret: str
+                The client secret provided by Spotify for authentication.
+            redirect_uri: str
+                The redirect URI where the response from Spotify will be sent.
+            scope: str
+                The scope of the access request.
+
+        Returns:
+            Dict[str, str]:
+                A dictionary containing the access and refresh token.
+
+        """
+        if self.spotify:
+            return None
+        if not self.spotify_oauth_manager:
+            self.spotify_oauth_manager = SpotifyOAuth(
+                client_id=client_id,
+                client_secret=client_secret,
+                redirect_uri=redirect_uri,
+                scope=scope,
+                open_browser=False,
+            )
+
+        auth_url = self.spotify_oauth_manager.get_authorize_url()
+        logger.info(f"Please go to this URL and authorize access: {auth_url}")
+
+        response_url = input("Paste the full redirect URL here: ")
+        code = self.spotify_oauth_manager.parse_response_code(response_url)
+
+        token = self.spotify_oauth_manager.get_access_token(code)
+        logger.info(f"Access Token: {token['access_token']}")
+        logger.info(f"Refresh Token: {token['refresh_token']}")
+
+        self.spotify = spotipy.Spotify(auth_manager=self.spotify_oauth_manager)
+
+        return token
+
+    def server_create_spotify(
+        self,
+        scope: str,
+        refresh_token: str,
+    ) -> None:
+        """Create a Spotify server connection using the provided OAuth details.
+
+        Args:
+            scope (str): The scope of access requested.
+            refresh_token (str): The refresh token to refresh the access token.
+
+        Returns:
+            None
+
+        """
+        if self.spotify:
+            return
+        if not self.spotify_oauth_manager:
+            self.spotify_oauth_manager = SpotifyOAuth(
+                scope=scope,
+                open_browser=False,
+            )
+
+        self.spotify_oauth_manager.refresh_access_token(refresh_token=refresh_token)
+        self.spotify = spotipy.Spotify(auth_manager=self.spotify_oauth_manager)
+
+    def get_current_song(self) -> str:
+        """Fetch the current song being played on Spotify.
+
+        Raises:
+            Exception: If the Spotify instance is not initialized.
+
+        Returns:
+            str: The name of the current song followed by the artist's name.
+                 If no song is currently playing, returns an empty string.
+
+        """
+        if not self.spotify:
+            raise Exception("Spotify not initialized")
+
+        if self.spotify.currently_playing():
+            return (
+                f"{self.spotify.currently_playing()['item']['name']}"
+                f" by "
+                f"{self.spotify.currently_playing()['item']['artists'][0]['name']}"
+            )
+        else:
+            return ""
